0cdd3c5d10624d618961aab5ea9f3f1b5b89086d
Ayaaa13 <98499445+Ayaaa13@users.noreply.github.com>
2025-04-25 08:07:54 +0800

    Update bfac.sql

--- database/bfac.sql
+++ database/bfac.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2025 at 04:06 AM
+-- Generation Time: Apr 25, 2025 at 02:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,7 +34,7 @@ CREATE TABLE `admin_accounts` (
   `password` varchar(250) NOT NULL,
   `email` varchar(255) NOT NULL,
   `role` enum('Admin','Treasurer') NOT NULL,
-  `profile_image` varchar(255) DEFAULT 'profile_images/default_profile.jpg',
+  `profile_image` varchar(255) DEFAULT 'admin_profile/default_profile.jpg',
   `is_archived` int(11) NOT NULL,
   `created_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -44,7 +44,7 @@ CREATE TABLE `admin_accounts` (
 --
 
 INSERT INTO `admin_accounts` (`user_id`, `full_name`, `username`, `password`, `email`, `role`, `profile_image`, `is_archived`, `created_by`) VALUES
-(1, 'Administrator', 'admin', '$2y$10$g5ArkrZmU72GNXwBecq5R..9qE1Egz6R56880UU.ZOnfr3klV7A1K', '', 'Admin', 'profile_images/default_profile.jpg', 0, NULL);
+(1, 'Administrator', 'admin', '$2y$10$g5ArkrZmU72GNXwBecq5R..9qE1Egz6R56880UU.ZOnfr3klV7A1K', 'admin@gmail.com', 'Admin', 'admin_profile/default_profile.jpg	', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -67,7 +67,11 @@ CREATE TABLE `admin_dividends` (
 INSERT INTO `admin_dividends` (`id`, `member_id`, `dividend_amount`, `receipt`, `calculation_date`) VALUES
 (1, 1, 30.00, '3616516189', '2023-04-22 20:16:39'),
 (2, 1, 50.00, '651616381', '2023-04-22 20:16:56'),
-(3, 1, 80.00, '646816', '2025-04-22 21:01:58');
+(3, 1, 80.00, '646816', '2025-04-22 21:01:58'),
+(5, 3, 93000000.00, '123', '2025-04-23 20:30:12'),
+(6, 3, 21586.00, '123', '2025-04-23 20:31:43'),
+(8, 3, 10000.00, '65165165', '2025-04-24 20:45:17'),
+(10, 2, 1200.00, '656516', '2025-04-24 21:31:02');
 
 -- --------------------------------------------------------
 
@@ -81,15 +85,17 @@ CREATE TABLE `admin_expenses` (
   `amount` decimal(10,2) NOT NULL,
   `expense_date` date NOT NULL,
   `description` text DEFAULT NULL,
-  `year` int(11) NOT NULL
+  `year` int(11) NOT NULL,
+  `is_archived` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin_expenses`
 --
 
-INSERT INTO `admin_expenses` (`id`, `category`, `amount`, `expense_date`, `description`, `year`) VALUES
-(1, 'Others', 200.00, '2024-04-22', 'Payment', 2024);
+INSERT INTO `admin_expenses` (`id`, `category`, `amount`, `expense_date`, `description`, `year`, `is_archived`) VALUES
+(1, 'Others', 200.00, '2024-04-22', 'Payment', 2024, 0),
+(2, 'Feeding', 600000.00, '2025-04-24', '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -118,16 +124,22 @@ CREATE TABLE `admin_sales` (
   `unitprice` varchar(50) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `receipt_no` varchar(50) NOT NULL,
-  `purchase_date` date NOT NULL
+  `purchase_date` date NOT NULL,
+  `is_archived` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin_sales`
 --
 
-INSERT INTO `admin_sales` (`id`, `sales_no`, `description`, `quantity`, `unitprice`, `amount`, `receipt_no`, `purchase_date`) VALUES
-(1, '1000', 'Product Name', 2, '200', 400.00, '616813205385', '2024-04-22'),
-(2, '1001', 'Example', 2, '100', 200.00, '6516516', '2025-04-23');
+INSERT INTO `admin_sales` (`id`, `sales_no`, `description`, `quantity`, `unitprice`, `amount`, `receipt_no`, `purchase_date`, `is_archived`) VALUES
+(1, '1000', 'Product Name', 2, '200', 400.00, '616813205385', '2024-04-22', 0),
+(2, '1001', 'Example', 2, '100', 200.00, '6516516', '2025-04-23', 0),
+(3, '1002', 'Product B', 12, '123', 1476.00, '222', '2025-04-24', 0),
+(4, '1003', 'Product A', 9, '23', 207.00, '123', '2025-04-23', 0),
+(5, '1004', 'Product A', 9, '12', 108.00, '3', '2025-04-24', 0),
+(6, '1005', 'Product A', 1, '1000000000', 99999999.99, '123', '2025-04-24', 0),
+(7, '1006', 'Product A', 1, '1400000000', 99999999.99, '3', '2025-04-24', 0);
 
 -- --------------------------------------------------------
 
@@ -147,7 +159,8 @@ CREATE TABLE `admin_shares` (
 --
 
 INSERT INTO `admin_shares` (`id`, `member_id`, `update_at`, `is_archived`) VALUES
-(1, 1, NULL, 0);
+(1, 2, NULL, 0),
+(2, 3, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -169,9 +182,8 @@ CREATE TABLE `admin_shares_list` (
 --
 
 INSERT INTO `admin_shares_list` (`id`, `member_id`, `paid_up_share_capital`, `share_capital`, `receipt_number`, `created_at`) VALUES
-(1, 1, 200.00, 2.00, '15643515', '2024-04-22 10:38:56'),
-(2, 1, 200.00, 2.00, '6162626', '2024-04-22 13:37:58'),
-(3, 1, 200.00, 2.00, '61616', '2025-04-22 14:56:07');
+(1, 2, 1200000.00, 12000.00, '351681516816', '2025-04-25 00:06:08'),
+(2, 3, 20000.00, 200.00, '65168168186', '2025-04-25 00:06:43');
 
 -- --------------------------------------------------------
 
@@ -275,7 +287,9 @@ CREATE TABLE `user_members` (
 --
 
 INSERT INTO `user_members` (`member_id`, `first_name`, `middle_name`, `last_name`, `username`, `email`, `password`, `phone`, `rsbsa_number`, `address`, `farm_location`, `role`, `profile_image`, `status`, `is_archived`, `is_verified`, `token`, `token_expiry`, `date_registered`) VALUES
-(1, 'Jeremiah', '', 'Nava', 'jnava', 'aiahnava5@gmail.com', '$2y$10$ZRBfSIMnuxDxuLp9o1Q6xOwiZKbM51NgaFISB6RX8w0rJdBX6t0lS', '09155434721', '', 'taga dito', 'Secret', 'User', 'profile_images/default_profile.jpg', 'Approved', 0, 1, NULL, '2025-04-22 13:29:16', '2025-04-22 10:29:21');
+(1, 'Jeremiah', '', 'Nava', 'jnava', 'aiahnava5@gmail.com', '$2y$10$ZRBfSIMnuxDxuLp9o1Q6xOwiZKbM51NgaFISB6RX8w0rJdBX6t0lS', '09155434721', '', 'taga dito', 'Secret', 'User', 'profile_images/default_profile.jpg', 'Approved', 0, 1, NULL, '2025-04-22 13:29:16', '2025-04-22 10:29:21'),
+(2, 'Diana Rose', 'Buenaflor', 'Nuique', 'dnuique', 'dianarosenuique4@gmail.com', '$2y$10$IExT/Tcsb9c7lRHxIvxE8.vYhBButUggU.huGREjpWjx3IBXRaNRu', '09271806732', '111', 'Hermosa', 'Camalig', 'User', 'profile_images/YGhcVybAOq43_DianaRose_Nuique.jpg', 'Approved', 0, 1, NULL, '2025-04-23 18:49:56', '2025-04-23 21:49:58'),
+(3, 'Lian', 'Marcos', 'Villanueva', 'lvillanueva', 'lianmvillanueva@gmail.com', '$2y$10$wLuvQbt9tlsWRoZEIkka1OWQZfMmNnexJm3mCjWNtm4swq41eR1eK', '09386012451', '', 'sitio camalig, tuyo, balanga city, bataan', 'Tuyo', 'User', 'profile_images/LpJ0slRoHh4j_Lian_Villanueva.jpg', 'Approved', 0, 1, NULL, NULL, '2025-04-24 02:50:27');
 
 -- --------------------------------------------------------
 
@@ -408,13 +422,13 @@ ALTER TABLE `admin_accounts`
 -- AUTO_INCREMENT for table `admin_dividends`
 --
 ALTER TABLE `admin_dividends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `admin_expenses`
 --
 ALTER TABLE `admin_expenses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `admin_login`
@@ -426,19 +440,19 @@ ALTER TABLE `admin_login`
 -- AUTO_INCREMENT for table `admin_sales`
 --
 ALTER TABLE `admin_sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `admin_shares`
 --
 ALTER TABLE `admin_shares`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `admin_shares_list`
 --
 ALTER TABLE `admin_shares_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `admin_users`
@@ -456,7 +470,7 @@ ALTER TABLE `user_financial_summary`
 -- AUTO_INCREMENT for table `user_members`
 --
 ALTER TABLE `user_members`
-  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_profiles`
